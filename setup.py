1c9f5466f72240f3353c41c7623cfa01c7b2b91c
maskina-is <urvinnsla@maskina.is>
2025-04-02 21:47:42 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  author = "Maskína",
+  description="Helper functions for Maskína",
+  name = "maskpy".
+  version="0.1.0",
+  packages=find_packages(include=["maskpy", "maskpy.*"])
+)
+
+  
